631ba8e58f6737b59658804341910b611832db74
CrisBMoya <tobal.92@gmail.com>
2019-07-18 15:52:26 -0400

    ud

--- MM.py
+++ MM.py
@@ -2,16 +2,17 @@ import pandas as pd
 import numpy as np
 import re
 import os
-import chart_studio.plotly as py #replaced by plotly.plotly as is deprecated according to some shit
+#import chart_studio.plotly as py #replaced by plotly.plotly as is deprecated according to some shit
 import plotly_express as px
 import plotly.graph_objs as go
 import math
+import plotly as py
 
 #Route
 DataFile=os.getcwd() + "/Data/MissingMigrants-Global-2019-03-29T18-36-07.csv"
 
 #Read data
-MM=pd.read_csv(filepath_or_buffer="C:/Users/Tobal/Google Drive/Github/MissingMigrants/Data/MissingMigrants-Global-2019-03-29T18-36-07.csv")
+MM=pd.read_csv(filepath_or_buffer="/home/tobal/GithubProjects/MissingMigrants/Data/MissingMigrants-Global-2019-03-29T18-36-07.csv")
 MM.head()
 
 
@@ -42,10 +43,8 @@ GOLatLon=GOLatLon.add_trace(go.Scattergeo(
         lat=MM["Lat"],
         mode="markers"
         ))
-py.iplot(GOLatLon)
-
+GOLatLon.show()
 #Use size of dead and missing as marker size and as color as well
 GOLatLonSize=px.scatter_geo(data_frame=MM, lat="Lat", lon="Lon", size="Total Dead and Missing", color="Total Dead and Missing")
-py.iplot(GOLatLonSize)
-
 GOLatLonSize.show()
+
